4adb0ececcaf8878a44db4030fdb801df915db76
Jonathan Hernandez Zapata <jhzapata.seven@gmail.com>
2022-10-03 08:59:16 -0500

    Adding a module module bundler and sass support

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import './styles/styles.scss';
+
 const $app = document.getElementById('app');
 const $observe = document.getElementById('observe');
 const API = 'https://api.escuelajs.co/api/v1/products';
