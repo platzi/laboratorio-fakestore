0c17f36d942b9edf4edc338166f2f441ba3d51d8
Pedro Andres Chaparro Quintero <pedroandreschaparro@gmail.com>
2022-12-23 09:02:09 -0500

    :sparkles: Infinite scroll with `IntersectionObserver`

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-import { getLocalStorageEntry } from './services/localstorage.services';
+import { getLocalStorageEntry, updateLocalStorageEntry } from './services/localstorage.services';
 import { getProductsFromPage } from './services/products.services';
 import { createProductCard } from './view/products.ui';
 
 const $app = document.getElementById('app');
-// const $observe = document.getElementById('observe');
-let currentPage = getLocalStorageEntry('pagination') || 1;
+const $observe = document.getElementById('observe');
 
-const loadProducts = async () => {
+updateLocalStorageEntry('pagination', 0);
+let currentPage = getLocalStorageEntry('pagination') || 0;
+
+const renderProducts = async () => {
   const [error, products] = await getProductsFromPage(currentPage, 10);
 
   if (!error) {
@@ -18,4 +20,18 @@ const loadProducts = async () => {
   }
 };
 
-loadProducts();
+const ObserverTrigger = (entries, _) => {
+  const { isIntersecting } = entries[0];
+
+  if (isIntersecting) {
+    currentPage++;
+    updateLocalStorageEntry('pagination', currentPage);
+    renderProducts(currentPage, 10);
+  }
+};
+
+const Observer = new IntersectionObserver(ObserverTrigger, {
+  treshold: 0.5,
+});
+
+Observer.observe($observe);
