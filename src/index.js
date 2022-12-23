b053d059098cf72e0114bdc0a6c4e23ca6bf3967
Pedro Andres Chaparro Quintero <pedroandreschaparro@gmail.com>
2022-12-23 08:50:14 -0500

    :construction: Initial functions to manage local storage

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
+import { getLocalStorageEntry } from './services/localstorage.services';
 import { getProductsFromPage } from './services/products.services';
 import { createProductCard } from './view/products.ui';
 
 const $app = document.getElementById('app');
-const $observe = document.getElementById('observe');
+// const $observe = document.getElementById('observe');
+let currentPage = getLocalStorageEntry('pagination') || 1;
 
 const loadProducts = async () => {
-  const [error, products] = await getProductsFromPage(1, 10);
+  const [error, products] = await getProductsFromPage(currentPage, 10);
 
   if (!error) {
     products.forEach((product) => {
