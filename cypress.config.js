b8c4c78e8c4b0be63f4a5bee280c19ca3bf1ca88
Santiago Alvarez <santiagoalvarez9617@gmail.com>
2023-02-20 13:37:15 -0500

    Enable cypress inside vite

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,14 @@
+const path = require('path');
 const { defineConfig } = require("cypress");
+const vitePreprocessor = require("cypress-vite");
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      on(
+        'file:preprocessor', 
+        vitePreprocessor(path.resolve(__dirname, './vite.config.js')))
     },
   },
 });
